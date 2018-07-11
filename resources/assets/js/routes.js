5c867cc0cdbb030c934d2f0a23018f76c47bfc36
Kutalo Alexey <kutalo84@gmail.com>
2018-07-11 00:53:54 -0300

    Fix document title when opening a page in new tab

--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -237,6 +237,7 @@ router.beforeEach((to, from, next) => {
             const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title);
             const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
             if (nearestWithTitle) document.title = nearestWithTitle.meta.title;
+            if (document.title.includes('undefined')) document.title = document.title.replace("undefined", helper.getConfig('company_name'));
             Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el));
             if (!nearestWithMeta) return next();
             nearestWithMeta.meta.metaTags.map(tagDef => {
